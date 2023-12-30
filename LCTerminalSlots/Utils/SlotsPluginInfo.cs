0bb18009335490f4f4140200e04c101db6aa7b2f
Jack Whatley <wickselm2005@gmail.com>
2023-12-30 18:31:32 +0000

    Actual Release v0.0.3

--- LCTerminalSlots/Utils/SlotsPluginInfo.cs
+++ LCTerminalSlots/Utils/SlotsPluginInfo.cs
@@ -4,7 +4,7 @@
     {
         internal const string MOD_GUID = "dev.jackwhatley.lcslots";
 
-        internal const string MOD_NAME = "Lethal Company Terminal Slots";
+        internal const string MOD_NAME = "Lethal Company Gambling Mod";
 
         internal const string MOD_VER = "0.0.3";
     }
